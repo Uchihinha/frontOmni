179a396516fb25821da92614b0da9976a3f546ef
Gabriel Jorge <gabriel.jorge.pereira@outlook.com>
2020-01-19 23:26:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './globals.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+import api from './services/api';
+
+import DevItem from './components/DevItem/index';
+import DevForm from './components/DevForm/index';
+
+// Componente 	--> Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade 	--> Informações que o componente PAI passa para o componente FILHO
+// Estado		--> Informações mantidas pelo componente (Imutabilidade JS)
+
+function App() { // JSX (JavaScript + HTML)
+
+	const [devs, setDevs] = useState([]);
+
+	useEffect(() => {
+		async function loadDevs() {
+			const response = await api.get('/devs');
+			setDevs(response.data.data);
+		};
+		
+		loadDevs();
+	}, [])
+
+	async function handleAddDev(e) {
+		const response = await api.post('/devs', e); 
+		setDevs([...devs, response.data.data]);
+	}
+
+	return (
+		<div id="app">
+			<aside>
+				<strong>Cadastrar</strong>
+				<DevForm onSubmit={handleAddDev} />
+			</aside>
+			<main>
+				<ul>
+					{devs.map(dev => (
+						<DevItem key={dev._id} dev={dev} />
+					))}
+					
+				</ul>
+			</main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/DevForm/index.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+
+function DevForm(props) {
+    const { onSubmit } = props;
+ 
+    const [latitude, setLatitude] = useState('');
+	const [longitude, setLongitude] = useState('');
+	const [github_username, setGithubUsername] = useState('');
+    const [techs, setTechs] = useState('');
+
+    useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				const { latitude, longitude } = position.coords;
+				setLatitude(latitude);
+				setLongitude(longitude);
+			},
+			(err) => {
+				console.log('DEU ERRADO');
+				console.log(err);
+			},{
+				timeout: 30000
+			}
+		);
+    }, []); // passar array vazio caso queira que seja executada uma única vez
+    
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        await onSubmit({
+            github_username,
+			techs,
+			latitude,
+			longitude
+        });
+
+        setGithubUsername('');
+		setTechs('');
+    }
+    
+    return(
+        <form onSubmit={handleSubmit}>
+            <div className="input-block"> 
+                <label htmlFor="github_username">Usuário do GitHub</label>
+                <input name="github_username" id="github_username" value={github_username} required onChange={e => setGithubUsername(e.target.value)}/>
+            </div>
+
+            <div className="input-block"> 
+                <label htmlFor="techs">Tecnologias</label>
+                <input name="techs" id="techs" value={techs} required onChange={e => setTechs(e.target.value)}/>
+            </div>
+
+            <div className="input-group">
+                <div className="input-block">
+                    <label htmlFor="latitude">Latitude</label>
+                    <input 
+                        type="number" 
+                        name="latitude" 
+                        id="latitude" 
+                        value={latitude} 
+                        required
+                        onChange={e => setLatitude(e.target.value)}
+                    />
+                </div>
+
+                <div className="input-block">
+                    <label htmlFor="longitude">Longitude</label>
+                    <input 
+                        type="number" 
+                        name="longitude" 
+                        id="longitude" 
+                        value={longitude} 
+                        required
+                        onChange={e => setLongitude(e.target.value)}
+                    />
+                </div>
+            </div>
+
+            <button type="submit">Salvar</button>
+
+        </form>
+    );
+}
+
+export default DevForm;
